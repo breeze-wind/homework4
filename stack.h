a27f9c9275eb7632ac0849374e16ee90ee2de8d7
sfx233 <sfx233@nuaa.edu.cn>
2024-12-23 22:42:18 +0800

    list刚开始

--- stack.h
+++ stack.h
@@ -4,11 +4,18 @@
 
 #ifndef STACK_H
 #define STACK_H
-
+#include "list.h"
+#include "stack.h"
 namespace my {
-
+template <typename T>
 class stack {
-
+public: stack();
+    ~stack()=default;
+      T* top() ;
+    void push(const T& data);
+    void pop(const T& data);
+private:
+    list<T> stacklist ;
 
 };
 
